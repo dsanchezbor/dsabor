ed0f8b411915fcf6bf977993c6ed04aabbdbf782
dsanchezbor <144958958+dsanchezbor@users.noreply.github.com>
2023-09-19 10:49:05 -0600

    Add files via upload

---
+++ hola mundo/hola_mundo.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author dsanchezbor
+ */
+public class hola_mundo {
+    
+    /**
+     *  @param args the command line arguments
+     */
+    public static void main(String[]args) {
+        // TODO code application logi here
+        System.out.println("Hola mundo");
+    }
+    
+}
